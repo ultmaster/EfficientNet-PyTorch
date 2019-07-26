c763ee5ff1cc44ec53c5b817f9357ad4a600a606
zhangyuge <scottyugochang@gmail.com>
2019-07-26 18:04:38 +0800

    checkpoint of gathering data

--- tools/analysis/gather_data.py
+++ tools/analysis/gather_data.py
@@ -1,39 +1,38 @@
+import json
 import os
 from argparse import ArgumentParser
+from datetime import timedelta
 
 import xlsxwriter
 
 
 def analyze_metrics(root_dir, worksheet):
-    # Some data we want to write to the worksheet.
-    expenses = (
-        ['Rent', 1000],
-        ['Gas', 100],
-        ['Food', 300],
-        ['Gym', 50],
-    )
+    columns = ["task_block_in_byte", "task_block_out_byte", "task_cpu_percent",
+               "task_gpu_mem_percent", "task_gpu_percent", "task_mem_usage_byte",
+               "task_net_in_byte", "task_net_out_byte"]
 
-    # Start from the first cell. Rows and columns are zero indexed.
-    row = 0
-    col = 0
+    for i, column in enumerate(columns, start=1):
+        worksheet.write(0, i, column)
 
-    # Iterate over the data and write it out row by row.
-    for item, cost in (expenses):
-        worksheet.write(row, col, item)
-        worksheet.write(row, col + 1, cost)
-        row += 1
-
-    # Write a total using a formula.
-    worksheet.write(row, 0, 'Total')
-    worksheet.write(row, 1, '=SUM(B1:B4)')
+    for trial_dir in os.listdir(os.path.join(root_dir, "pai")):
+        trial_dir = os.path.join(root_dir, "pai", trial_dir)
+        for i, column in enumerate(columns, start=1):
+            with open(os.path.join(trial_dir, column + ".json"), "r") as fp:
+                data = json.load(fp)
+            data = data["data"]["result"][0]["values"]
+            if not data:
+                continue
+            start_time = data[0][0]
+            for j, (time, val) in enumerate(data, start=1):
+                worksheet.write_datetime(j, 0, timedelta(seconds=time - start_time))
+                worksheet.write_number(j, i, val)
 
 
 
 def main(root_dir):
     workbook = xlsxwriter.Workbook(os.path.join(root_dir, 'result.xlsx'))
-
     metrics = workbook.add_worksheet("metrics")
-
+    analyze_metrics(root_dir, metrics)
     workbook.close()
 
 
