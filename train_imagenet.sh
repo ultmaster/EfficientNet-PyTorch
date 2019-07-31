b15e9563382d931478e2abad0b7f0baeafd50e63
zhangyuge <scottyugochang@gmail.com>
2019-07-31 19:15:03 +0800

    ready for imagenet efficientnet

--- train_imagenet.sh
+++ train_imagenet.sh
@@ -4,4 +4,4 @@ mkdir --parents /mnt/data
 mount -t cifs //10.151.40.4/data/yugzh /mnt/data -o vers=3.0,username=paismbuser,password=paismbpwd,domain=WORKGROUP
 
 # (tensorboard --logdir /tmp/summary --port $PAI_PORT_LIST_main_0_tensorboard &)
-python3 main.py /mnt/data/imagenet -j 16 --epochs 5 -a efficientnet --num-classes 1000 --batch-size 64 --optimizer rmsprop --lr 0.064 --wd 1e-5 --request-from-nni
+python3 main.py /mnt/data/imagenet -j 16 --epochs 5 -a efficientnet --num-classes 1000 --batch-size 32 --optimizer rmsprop --lr 0.032 --wd 1e-5 --request-from-nni
