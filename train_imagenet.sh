4cb7384f2d8e531a0e9681cfc465acb505039d7b
Yuge Zhang <scottyugochang@gmail.com>
2019-10-14 11:27:22 +0800

    Update train_imagenet.sh

--- train_imagenet.sh
+++ train_imagenet.sh
@@ -5,5 +5,4 @@ mkdir --parents /mnt/data
 mount -t cifs //10.151.41.13/data/yugzh /mnt/data -o vers=3.0,username=paismbuser,password=paismbpwd,domain=WORKGROUP
 git clone https://github.com/ultmaster/EfficientNet-PyTorch
 cd EfficientNet-PyTorch
-cp examples/imagenet/main.py .
 python3 main.py /mnt/data/imagenet -j 1 -a efficientnet --batch-size 32 --lr 0.032 --wd 1e-5 --epochs 5 --request-from-nni
