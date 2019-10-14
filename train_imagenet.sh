582a0b29e47bf9b9f982666898c46ed0c0f01fef
Yuge Zhang <scottyugochang@gmail.com>
2019-10-14 11:52:03 +0800

    Update train_imagenet.sh

--- train_imagenet.sh
+++ train_imagenet.sh
@@ -1,7 +1,6 @@
 set -x
 apt update
 apt install --assume-yes nfs-common cifs-utils sshpass wget git
-python3 -m pip install tensorboardx
 mkdir --parents /mnt/data
 mount -t cifs //10.151.41.14/data/yugzh /mnt/data -o vers=3.0,username=paismbuser,password=paismbpwd,domain=WORKGROUP
 git clone https://github.com/ultmaster/EfficientNet-PyTorch
