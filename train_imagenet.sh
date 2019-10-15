62f3088c287a48515b1ea204584dce2943100f29
Yuge Zhang <scottyugochang@gmail.com>
2019-10-15 13:20:29 +0800

    Update train_imagenet.sh

--- train_imagenet.sh
+++ train_imagenet.sh
@@ -2,7 +2,7 @@ set -x
 apt update
 apt install --assume-yes nfs-common cifs-utils sshpass wget git
 mkdir --parents /mnt/data
-mount -t cifs //10.151.41.14/data/yugzh /mnt/data -o vers=3.0,username=paismbuser,password=paismbpwd,domain=WORKGROUP
+mount -t cifs //path/to/folder/contains/imagenet -o vers=3.0,username=username,password=password,domain=WORKGROUP  # mount imagenet
 git clone https://github.com/ultmaster/EfficientNet-PyTorch
 cd EfficientNet-PyTorch
 python3 main.py /mnt/data/imagenet -j 1 -a efficientnet --batch-size 32 --lr 0.032 --wd 1e-5 --epochs 5 --request-from-nni
